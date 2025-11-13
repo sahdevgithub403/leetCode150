24acaed03a77cc23bcf7460f99db2ce5d5f1853f
sahdevgithub403 <sahdevpuran192@gmail.com>
2025-11-13 23:21:49 +0530

    ad problem

---
+++ TwoPointer/ThreeSum.java
@@ -0,0 +1,11 @@
+public class ThreeSum {
+
+    public int threeSum(int[] nums){
+        return threeSum(nums);
+    }
+    public static void main(String[] args) {
+        ThreeSum ts = new ThreeSum();
+        int[] nums = {1,3,4,5,9};
+        ts.threeSum(nums);
+    }
+}
